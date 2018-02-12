b9dafbde4f0dc3f33cbee909197988d0d032a962
Purinda Gunasekara <purinda.gunasekara@openbet.com>
2018-02-12 23:31:02 +1100

    Cleared blynk micro python project

--- esp8266/weather-station-blynk/boot.py
+++
@@ -1,8 +0,0 @@
-import network
-import gc
-from Parameters import RouterConfig
-
-sta_if = network.WLAN(network.STA_IF)
-sta_if.active(True)
-sta_if.connect(RouterConfig.SSID, RouterConfig.PW) # Connect to an AP
-gc.collect()
--- esp8266/weather-station-blynk/deploy.sh
+++
@@ -1,8 +0,0 @@
-echo "Copying application to ESP8266..."
-ampy --port /dev/tty.SLAB_USBtoUART put ../../config/global_config.py
-ampy --port /dev/tty.SLAB_USBtoUART put boot.py
-ampy --port /dev/tty.SLAB_USBtoUART put simple.py
-ampy --port /dev/tty.SLAB_USBtoUART put robust.py
-ampy --port /dev/tty.SLAB_USBtoUART put main.py
-ampy --port /dev/tty.SLAB_USBtoUART put bme280.py
-echo "Done copying."
